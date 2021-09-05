e2fcd0ebd1be6e6a3258dd93bbebd64824e754ce
Saurav Sahu <mrsauravsahu@outlook.com>
2021-09-05 12:28:25 +0530

    Feat: SSR docs site (#4)
    * remove frontend project
    
    * create sapper project
    
    * move services into a folder
    
    * ignore .envrc
    
    * add template .envrc for secrets
    
    * update readme

--- frontend/jest.config.js
+++
@@ -1,6 +0,0 @@
-const presetJestConfigGenerator = require('@snowpack/app-scripts-react/jest.config.js');
-
-module.exports = {
-  ...presetJestConfigGenerator(),
-  testMatch: ['**/__tests__/**/*.[jt]s?(x)'],
-};
--- frontend/src/index.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { Home } from './pages/home/home';
-
-ReactDOM.render(<Home />, document.querySelector('main'));
